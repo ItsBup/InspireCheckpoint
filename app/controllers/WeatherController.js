687a40bf83b3b1516fae808e09872181e10a3072
Kaden <kjrigsbee@gmail.com>
2023-12-15 11:27:45 -0700

    pizza time

--- app/controllers/WeatherController.js
+++ app/controllers/WeatherController.js
@@ -1,8 +1,19 @@
-
+import { weatherService } from "../services/WeatherService.js";
+import { Pop } from "../utils/Pop.js";
 
 export class WeatherController {
   constructor() {
     console.log('Weather Loaded')
+    this.getWeatherApi()
+  }
+
+  async getWeatherApi(){
+    try {
+      await weatherService.getWeatherApi()
+    } catch (error) {
+      console.error(error);
+      Pop.error(error)
+    }
   }
 
 }
--- app/services/WeatherService.js
+++ app/services/WeatherService.js
@@ -1,6 +1,10 @@
+import { api } from "./AxiosService.js"
 
 class WeatherService {
-
+    async getWeatherApi(){
+        const response = await api.get('api/weather')
+        console.log('pizza time', response)
+    }
 }
 
 export const weatherService = new WeatherService()
